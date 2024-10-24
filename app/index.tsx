10e0a2da98d2b8ee3d99ec6a70040314b06348f0
Dragon00716 <pmg76dayo@gmail.com>
2024-10-24 13:59:49 +0900

    ディレクトリ構成決定

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,28 @@
-import { Text, View } from "react-native";
+// import { Text, View, Button } from "react-native";
+// import { useRouter } from "expo-router";
+
+// export default function Index() {
+//   const router = useRouter();
+
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
+//     >
+//       <Text>hello world my name is tomo tin desu!</Text>
+//       <Text>Edit app/index.tsx to edit this screen.</Text>
+
+//       {/* ボタンを追加して遷移 */}
+//       <Button title="Go to Test Screen" onPress={() => router.push("../frontend/test")} />
+//     </View>
+//   );
+// }
+
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>hello world my name is tomo tin desu!</Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  return <Redirect href={"../frontend/test"}/>;
 }
